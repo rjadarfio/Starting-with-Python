b63b8f50ce40162c24cdf45430996202cd66f92f
Roddy Adarfio <97347071+rjadarfio@users.noreply.github.com>
2022-01-09 20:05:45 -0400

    Add files via upload

---
+++ ex02-02.py
@@ -0,0 +1,8 @@
+hours=input("Enter the hours:")
+rate=input("Enter the pay rate:")
+rate=float(rate)
+hours=float(hours)
+pay = rate * hours
+print("Hours:", hours)
+print("Rate:", rate)
+print("Pay:", pay)
---
+++ ex02-03.py
@@ -0,0 +1,4 @@
+cel=input("Enter the temperature in Celsius:")
+cel=float(cel)
+far = (cel*9/5)+32
+print(far,"°Fahrenheit")
